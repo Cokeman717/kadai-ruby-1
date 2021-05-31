6a1b83a8167723fb226f090ff0b506c9c65f8918
cokeman <cokeredder@gmail.com>
2021-05-31 09:57:40 +0000

    kadai-rubyu-1

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,6 +1,4 @@
-firstname = '佑輔'  #名前の変数
 lastname = '澤井'   #苗字の変数　
+firstname = '佑輔'  #名前の変数
 age = 34    #年齢の変数
-strname = lastname + firstname  + 'です。'   #氏名を連結する演算子
-strage = age.to_s + '歳です。'  #年齢を文字列に変換
-puts strname + strage   #画面に表示
+puts lastname + firstname + "です。" + age.to_s + "歳です。"   #画面に表示
